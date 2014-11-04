05d210004a765f5e797192efc868fcb4ed944c05
ravaliB <benjamin.ravalijaona@gmail.com>
2014-11-04 01:28:46 +0100

    rectification sur recuit

--- src/observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package observer;
-
-import java.awt.Point;
-import java.util.HashMap;
-
-public interface Observer {
-	public void update(HashMap<Integer, Point> map, HashMap<Point, Point> linksMap, double temperature);
-}
